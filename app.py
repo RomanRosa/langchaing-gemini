ec6e53f109ec78f1877e9ec69efcf065f6625503
Roman de la Rosa <mldevops2@gmail.com>
2024-01-13 14:06:01 -0600

    correct faiss for FAISS in dependencies

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
-
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-
-from langchain.vectorstores import faiss
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -19,7 +17,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 def get_pdf_text(pdf_docs):
     text= ''
     for pdf in pdf_docs:
-        pdf_reader= PdfFileReader(pdf)
+        pdf_reader= PdfReader(pdf)
         for page in pdf_reader.pages:
             text+=page.extract_text()
     return text
