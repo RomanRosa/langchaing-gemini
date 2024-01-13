b265dba2c7989c4d2ddbdbc67cf87a83140f2e04
Roman de la Rosa <mldevops2@gmail.com>
2024-01-13 13:43:40 -0600

    adding user_input function

--- app.py
+++ app.py
@@ -50,3 +50,20 @@ def get_conversational_chain():
     prompt= PromptTemplate(template=prompt_template, input_variables=['context','question'])
     chain= load_qa_chain(model,chain_type='stuff',promt=prompt)
     return chain
+
+
+def user_input(user_question):
+    embeddings= GoogleGenerativeAIEmbeddings(model='models/embedding-001')
+
+    new_db= faiss.load_local('faiss_index', embeddings)
+    docs= new_db.similarity_search(user_question)
+
+    chain= get_conversational_chain()
+
+    response= chain(
+        {
+            'input_documents':docs, 'question': user_question}
+            , return_only_outputs=True)
+    
+    print(response)
+    st.write('Reply: ', response['output_text'])
