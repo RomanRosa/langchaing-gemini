4a50ee0038654860bb8dd364ce719317a30d059c
Roman de la Rosa <mldevops2@gmail.com>
2024-01-13 14:10:49 -0600

    update requirements.txt and app.py files, refactor applied

--- app.py
+++ app.py
@@ -14,79 +14,93 @@ load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+
+
+
+
+
 def get_pdf_text(pdf_docs):
-    text= ''
+    text=""
     for pdf in pdf_docs:
         pdf_reader= PdfReader(pdf)
         for page in pdf_reader.pages:
-            text+=page.extract_text()
-    return text
+            text+= page.extract_text()
+    return  text
+
 
 
 def get_text_chunks(text):
-    text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks= text_splitter.split_text(text)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
     return chunks
 
 
 def get_vector_store(text_chunks):
-    embeddings= GoogleGenerativeAIEmbeddings(model='models(embeddings-001)')
-    vector_store=faiss.from_text(text_chunks, embeddings=embeddings)
-    vector_store.save_local('faiss_index')
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
 
 
 def get_conversational_chain():
-    prompt_template= """
-    Answer the question as detailed as possibl from the provided context, make sure to provide all the details, if the answer is not in
-    the provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
     Context:\n {context}?\n
-    Question: \n {question}\n
+    Question: \n{question}\n
 
     Answer:
     """
-    model=ChatGoogleGenerativeAI(model='gemini-pro', temperature=0.3)
-    prompt= PromptTemplate(template=prompt_template, input_variables=['context','question'])
-    chain= load_qa_chain(model,chain_type='stuff',promt=prompt)
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
     return chain
 
 
+
 def user_input(user_question):
-    embeddings= GoogleGenerativeAIEmbeddings(model='models/embedding-001')
-
-    new_db= faiss.load_local('faiss_index', embeddings)
-    docs= new_db.similarity_search(user_question)
-
-    chain= get_conversational_chain()
-
-    response= chain(
-        {
-            'input_documents':docs, 'question': user_question}
-            , return_only_outputs=True)
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
     print(response)
-    st.write('Reply: ', response['output_text'])
+    st.write("Reply: ", response["output_text"])
 
 
 
-    def main():
-        st.set_page_config('Chat With Multiple PDF')
-        st.header('Chat with Multiple PDF Using Gemini💁')
 
-        user_question= st.text_input('Ask a Question from the PDF Files')
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
 
-        if user_question:
-            user_input(user_question)
+    user_question = st.text_input("Ask a Question from the PDF Files")
 
-        with st.sidebar:
-            st.title('Menu:')
-            pdf_docs= st.file_uploader('Upload your PDF Files and Click on the Submit & Process Button', accept_multiple_files=True)
-            if st.button('Submit & Process'):
-                with st.spinner('Processing...'):
-                    raw_text= get_pdf_text(pdf_docs)
-                    text_chunks= get_text_chunks(raw_text)
-                    get_vector_store(text_chunks)
-                    st.success('Done')
+    if user_question:
+        user_input(user_question)
 
-    
-    if __name__ == "__main__":
-        main()
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
