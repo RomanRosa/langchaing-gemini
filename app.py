6d07895b1f7e57316c7d2582d890ae36e4e22053
Roman de la Rosa <mldevops2@gmail.com>
2024-01-13 13:59:29 -0600

    adding streamlit main function

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
-
-genai.configure(api_key= os.getenv('GOOGLE_API_KEY'))
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_pdf_text(pdf_docs):
     text= ''
@@ -67,3 +67,28 @@ def user_input(user_question):
     
     print(response)
     st.write('Reply: ', response['output_text'])
+
+
+
+    def main():
+        st.set_page_config('Chat With Multiple PDF')
+        st.header('Chat with Multiple PDF Using Gemini💁')
+
+        user_question= st.text_input('Ask a Question from the PDF Files')
+
+        if user_question:
+            user_input(user_question)
+
+        with st.sidebar:
+            st.title('Menu:')
+            pdf_docs= st.file_uploader('Upload your PDF Files and Click on the Submit & Process Button', accept_multiple_files=True)
+            if st.button('Submit & Process'):
+                with st.spinner('Processing...'):
+                    raw_text= get_pdf_text(pdf_docs)
+                    text_chunks= get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success('Done')
+
+    
+    if __name__ == "__main__":
+        main()
