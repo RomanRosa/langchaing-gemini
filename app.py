51ea7d1f5a44c2d065fa87bfa49b8bd1e6599b11
Roman de la Rosa <mldevops2@gmail.com>
2024-01-13 13:18:08 -0600

    adding get_vector_store function

--- app.py
+++ app.py
@@ -29,3 +29,9 @@ def get_text_chunks(text):
     text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks= text_splitter.split_text(text)
     return chunks
+
+
+def get_vector_store(text_chunks):
+    embeddings= GoogleGenerativeAIEmbeddings(model='models(embeddings-001)')
+    vector_store=faiss.from_text(text_chunks, embeddings=embeddings)
+    vector_store.save_local('faiss_index')
